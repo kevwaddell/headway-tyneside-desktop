56f8efc1fbd2851e604d8469d4381b8e891a018b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-04-17 13:41:16 +0100

    Add tag manager script

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
+<?php if ($_SERVER['SERVER_NAME']=='http://www.headwaytyneside.com') { ?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRQV2XS');</script>
+<!-- End Google Tag Manager -->
+<?php } ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content ="width=device-width,user-scalable=yes">
 <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -9,6 +18,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php if ($_SERVER['SERVER_NAME']=='http://www.headwaytyneside.com') { ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRQV2XS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->	
+<?php } ?>
 <div id="page" class="site">
 	<a class="sr-only" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
