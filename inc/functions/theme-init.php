e52f1cc3c792a5388bff22fa687f0bff90b7fe3c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-04-18 14:54:45 +0100

    Add downloads page and regular meetings page

--- inc/functions/theme-init.php
+++ inc/functions/theme-init.php
@@ -7,7 +7,7 @@ if (!is_admin()) {
 		// Load stylesheets.
 		//wp_enqueue_style( 'print-styles', get_template_directory_uri().'/_/css/print-styles.css', null, filemtime( get_template_directory().'/_/css/print-styles.css' ), 'print' );
 		wp_enqueue_style( 'external', get_template_directory_uri().'/assets/css/external.css', null, filemtime( get_template_directory().'/assets/css/external.css' ), 'screen' );
-		wp_enqueue_style( 'styles', get_stylesheet_directory_uri().'/assets/css/styles.css', array('external'), filemtime( get_stylesheet_directory().'/assets/css/styles.css' ), 'screen' );
+		wp_enqueue_style( 'styles', get_stylesheet_directory_uri().'/assets/css/styles.css', array('external'), filemtime( get_stylesheet_directory().'/assets/less/styles.less' ), 'screen' );
 		
 		// Load JS
 		$functions_dep = array(
